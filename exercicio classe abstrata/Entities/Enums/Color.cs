4e96f01220a55430091a7ed738c4a964bcdea5f9
Davi Costa <davyda199@gmail.com>
2021-07-14 15:05:30 -0300

    Feito, faltando output

---
+++ exercicio classe abstrata/Entities/Enums/Color.cs
@@ -0,0 +1,9 @@
+﻿namespace exercicio_classe_abstrata.Entities
+{
+    enum Color:int
+    {
+        Black=1,
+        Blue=2,
+        Red=3
+    }
+}
---
+++ exercicio classe abstrata/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using exercicio_classe_abstrata.Entities;
+using System.Collections.Generic;
+namespace exercicio_classe_abstrata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Shape> list = new List<Shape>();
+            Console.WriteLine("Enter the number of shapes: ");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                Console.WriteLine($"Shape #{i} data: ");
+
+                Console.Write("Rectangle or Circle (r/c)? ");
+                char op = char.Parse(Console.ReadLine().ToLower());
+                Console.Write("Color (Black/Blue/Red)? ");
+                Color color = Enum.Parse<Color>(Console.ReadLine());
+                if (op == 'r')
+                {
+                    Console.Write("Width: ");
+                    double width = double.Parse(Console.ReadLine());
+                    Console.Write("Heigth: ");
+                    double heigth = double.Parse(Console.ReadLine());
+                    list.Add(new Rectangle(color,width, heigth));
+                }
+                else if (op == 'c')
+                {
+                    Console.Write("Radius: ");
+                    double radius = double.Parse(Console.ReadLine());
+                    list.Add(new Circle(color,radius));
+                }
+            }
+            foreach (Shape x in list)
+            {
+                x.Area();
+            }
+            Console.WriteLine("SHAPE AREAS: ");
+        }
+    }
+}
